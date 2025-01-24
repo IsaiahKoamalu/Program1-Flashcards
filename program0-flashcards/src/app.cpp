2ed8be7a228b274bfe8e5cd68df12dc4a4d6c6a5
Isaiah Broderson <161393413+IsaiahKoamalu@users.noreply.github.com>
2025-01-23 14:56:21 -1000

    Update app.cpp
    Changed the while loop parameter from a boolean 'running = true' to a char 'input != 113'.

--- program0-flashcards/src/app.cpp
+++ program0-flashcards/src/app.cpp
@@ -5,44 +5,46 @@
 #include <cstdlib>
 #include <ctime>
 
+//Initiallize Vector of Card objects
 FlashcardsApp::FlashcardsApp(std::string fileName) {
-  std::ifstream file(fileName);
-  if(!file.is_open()) {
-    std::cerr << "Error: Problem Opening File->" << fileName << std::endl;//Error Handling
-  }
+    std::ifstream file(fileName);
+    if (!file.is_open()) {
+        std::cerr << "Error: Problem Opening File->" << fileName << std::endl; //Error Handling
+    }
 
-  std::string current;
-  std::string next;
+    std::string current;
+    std::string next;
 
-  while(std::getline(file, current, '\t')) {
-    std::getline(file, next);
-    Card card(current, next);
-    flashCards.push_back(card);
-  }
+    while (std::getline(file, current, '\t')) {
+        std::getline(file, next);
+        Card card(current, next);
+        flashCards.push_back(card);
+    }
 }
 
+
+//Function that contains the main loop logic for the program
 void FlashcardsApp::run() {
-  srand(time(0));
+    srand(time(0)); //seed the random integer generator
 
-  std::string input;
+    char input;
 
-  bool running = true;
-  while(running) {
-    int randomElement = rand() % flashCards.size() + 0;
+    while (input != 113) { //Loop will run while the value of input char is not equal to the ASCII value of q(quit).
 
-    std::string currentFront = flashCards[randomElement].getFront();
-    std::string currentBack = flashCards[randomElement].getBack();
+        int randomElement = rand() % flashCards.size() + 0; //generate random integer in range of 0 -> Size of Cards vector.
 
-    std::cout << "\nFront: " << currentFront << std::endl;
-    std::cin >> input;
-    if(input == "q")
-      running = false;
-    else
-      std::cout << "Back: " << currentBack << std::endl;
-      std::cin >> input;
+        //Set temporary front and back strings to the current Card using the previously generated random number.
+        std::string currentFront = flashCards[randomElement].getFront();
+        std::string currentBack = flashCards[randomElement].getBack();
 
-  }
+        std::cout << "\nFront: " << currentFront << std::endl;
+        std::cin.get(input);
+        if (input == 10)
+            std::cout << "Back: " << currentBack << std::endl;
+        std::cin.get(input);
+    }
 }
+
 /*
 Implement the Flashcards App constructor.
 The constructor should read the TSV file passed to the constructor and store it in a vector of Card
